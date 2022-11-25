0f4f762c908ebda00da560565e53a3660fa5505c
Thalles Leite <eng.thallesleite@gmail.com>
2022-11-25 19:12:22 -0300

    Soma de todos os valores.

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+console.log("1. Percorra o array imprimindo todos os valores nele contidos com a função console.log();")
 
 console.log("\nUtilizando apenas o laço de repetição for")
 for (let i = 0; i < numbers.length; i += 1) {
@@ -18,3 +19,11 @@ while (indice < numbers.length) {
   console.log(numbers[indice])
   indice += 1;
 }
+
+console.log("\n2. Some todos os valores contidos no array e imprima o resultado:\n")
+
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i]
+}
+console.log(soma)

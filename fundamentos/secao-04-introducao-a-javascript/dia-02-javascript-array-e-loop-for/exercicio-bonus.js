0349a8f320ec5582e5a8bbcd9613e8e68137869c
Thalles Leite <eng.thallesleite@gmail.com>
2022-11-27 11:11:46 -0300

    Ordenação dos valores do vetor em ordem crescente

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus.js
@@ -0,0 +1,17 @@
+console.log("Ordene o array numbers em ordem crescente:")
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length - 1; i += 1) {
+
+
+  for (let j = 0; j < numbers.length - i - 1; j += 1) {
+
+    if (numbers[j] > numbers[j + 1]) {
+      aux = numbers[j + 1]
+      numbers[j + 1] = numbers[j]
+      numbers[j] = aux
+    }
+  }
+}
+console.log(numbers)

5e00bc9aff623dc3f3c9f311cb4d3250ba40bf01
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-08 22:52:13 -0300

    Acesso ao \'primeiroFilho\'

--- fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
+++ fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
@@ -6,5 +6,10 @@ paiOndeEstou.style.backgroundColor = 'yellow';
 const filhoOndeEstou = document.getElementById('elementoOndeVoceEsta').children[0];
 filhoOndeEstou.innerHTML = 'Texto exemplo.'
 
+const primeiroFilho = document.getElementById('pai').children[0];
+const primeiroFilho2 = document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+
+console.log(primeiroFilho2)
+console.log(primeiroFilho)
 console.log(paiOndeEstou)
 console.log(ondeEstou)

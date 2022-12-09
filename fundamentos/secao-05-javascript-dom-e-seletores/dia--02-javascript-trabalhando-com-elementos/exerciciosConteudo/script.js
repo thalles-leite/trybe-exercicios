5f662dc8f91354fa7510a928002ddbd3c2b6a812
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-08 22:56:05 -0300

    Exercicio finalizado.

--- fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
+++ fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
@@ -8,7 +8,13 @@ filhoOndeEstou.innerHTML = 'Texto exemplo.'
 
 const primeiroFilho = document.getElementById('pai').children[0];
 const primeiroFilho2 = document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+const atencao = document.getElementById('elementoOndeVoceEsta').nextSibling;
+const terceiroFilho = document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+const terceiroFilho2 = document.getElementById('pai').children[2];
 
+console.log(terceiroFilho2)
+console.log(terceiroFilho)
+console.log(atencao)
 console.log(primeiroFilho2)
 console.log(primeiroFilho)
 console.log(paiOndeEstou)

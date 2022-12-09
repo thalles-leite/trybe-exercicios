a1fc1b03e2334041012b69e9c8a4ce5afc8c11e9
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-08 22:41:23 -0300

    Acesso ao elemento primeiroFilhoDoFilho e texto adicionado a ele.

--- fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
+++ fundamentos/secao-05-javascript-dom-e-seletores/dia--02-javascript-trabalhando-com-elementos/exerciciosConteudo/script.js
@@ -1,7 +1,10 @@
 const ondeEstou = document.getElementById('elementoOndeVoceEsta');
 
-const paiOndeEstou = document.getElementById('elementoOndeVoceEsta').parentNode
+const paiOndeEstou = document.getElementById('elementoOndeVoceEsta').parentNode;
 paiOndeEstou.style.backgroundColor = 'yellow';
+
+const filhoOndeEstou = document.getElementById('elementoOndeVoceEsta').children[0];
+filhoOndeEstou.innerHTML = 'Texto exemplo.'
 
 console.log(paiOndeEstou)
 console.log(ondeEstou)

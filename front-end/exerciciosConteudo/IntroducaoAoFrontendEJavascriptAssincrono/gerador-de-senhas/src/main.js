f691e11e8543da6d8c8d834df207d5e9dd534ffa
Thalles Leite <eng.thallesleite@gmail.com>
2023-02-08 17:14:07 -0300

    Outra forma de copiuar no clipboard.

--- front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
+++ front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
@@ -1,21 +1,13 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 
 import './style.css';
 
 const botao = document.querySelector('button');
 const h2 = document.querySelector('h2');
 
-const copyToClipboard = async (texto) => {
-  try {
-    await navigator.clipboard.writeText(texto);
-    console.log('Pass copiado para o clipboard');
-  } catch (error) {
-    console.error('Erro ao copiar.', error);
-  }
-};
-
 botao.addEventListener('click', () => {
   const randomPass = nanoid();
   h2.innerText = randomPass;
-  copyToClipboard(randomPass);
+  copy(randomPass);
 });

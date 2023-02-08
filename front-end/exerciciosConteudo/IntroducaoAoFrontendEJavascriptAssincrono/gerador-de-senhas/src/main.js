cbb378d785e4edbad7423fc22de4e455fbc5bfd1
Thalles Leite <eng.thallesleite@gmail.com>
2023-02-08 12:04:22 -0300

    Finalização do app.

--- front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
+++ front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
@@ -1,1 +1,7 @@
-console.log('hello world.');
+import {nanoid} from 'nanoid'
+
+const botao = document.querySelector('button');
+const h2 = document.querySelector('h2');
+botao.addEventListener('click',() => {
+  h2.innerText = nanoid();
+})

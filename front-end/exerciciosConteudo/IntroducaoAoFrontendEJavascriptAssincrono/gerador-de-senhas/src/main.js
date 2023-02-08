4c04e2d85e67c6d253270bc23bba96c388b50d6a
Thalles Leite <eng.thallesleite@gmail.com>
2023-02-08 16:39:51 -0300

    Adição da feature que salva no clipboard a senha.

--- front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
+++ front-end/exerciciosConteudo/IntroducaoAoFrontendEJavascriptAssincrono/gerador-de-senhas/src/main.js
@@ -4,7 +4,18 @@ import './style.css';
 
 const botao = document.querySelector('button');
 const h2 = document.querySelector('h2');
+
+const copyToClipboard = async (texto) => {
+  try {
+    await navigator.clipboard.writeText(texto);
+    console.log('Pass copiado para o clipboard');
+  } catch (error) {
+    console.error('Erro ao copiar.', error);
+  }
+};
+
 botao.addEventListener('click', () => {
   const randomPass = nanoid();
   h2.innerText = randomPass;
+  copyToClipboard(randomPass);
 });

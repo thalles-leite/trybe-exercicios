c8f9fbdca1661a0194d81f77b4f8a97c667fb682
Thalles Leite <eng.thallesleite@gmail.com>
2023-06-16 23:47:13 -0300

    Atividade 1 concluída.

--- back-end/express_middlewares/src/app.js
+++ back-end/express_middlewares/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
+const auth = require('./middlewares/auth');
 
+const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -20,10 +22,22 @@ app.post(
   validateCreatedAt,
   validateRating,
   validateDifficulty,
+  auth,
 
   (_req, res) => {
     res.status(201).json({ 'message:': 'Atividade cadastrada com sucesso.' });
   },
 );
+
+app.post('/signup', (req, res) => {
+  const {
+    email, password, firstName, phone,
+  } = req.body;
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(400).json({ message: 'Campos ausentes!' });
+  }
+  const token = generateToken();
+  return res.status(200).json({ token });
+});
 
 module.exports = app;
